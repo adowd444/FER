3965faba5c845664d1919e972563cf7d51647e62
Ashley <92960881+adowd444@users.noreply.github.com>
2021-11-02 14:39:16 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-import keras
+from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Dense, Activation, Dropout, Flatten
